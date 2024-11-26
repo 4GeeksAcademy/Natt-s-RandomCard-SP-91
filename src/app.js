c6d7db4ca7812fd57c747a71f9ed298b1ff9871a
Natalia Manzano Carnerero <mcn.sdevelop@gmail.com>
2024-11-26 12:24:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let suits = ["heart", "spade", "diamond", "club"];
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "A",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  let card = document.querySelector(".custom-card");
+  card.classList.add(randomSuit);
+
+  document.querySelector(".number").textContent = randomNumber;
 };
